ab72669b0dd5db062fe4a308ae62e7894fc39606
jonatasmaxi <maximianojonatas@gmail.com>
2018-04-27 11:39:36 -0300

    Improve exception treatment

--- Java/TLS.java
+++ Java/TLS.java
@@ -11,6 +11,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,27 +20,32 @@ import java.net.URL;
  */
 public class TLS {
 
-
-    public static String testConnection () {
+    public static String testConnection() {
         StringBuilder result = new StringBuilder();
+        URL url;
         try {
-            URL url = new URL("https://tls12.pagar.me");
+            url = new URL("https://tls12.pagar.me");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
-            BufferedReader rd = new BufferedReader(new InputStreamReader (conn.getInputStream()));
+            conn.connect();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line;
-            while((line = rd.readLine()) !=null){
+            while ((line = rd.readLine()) != null) {
                 result.append(line);
             }
             rd.close();
             return result.toString();
         } catch (MalformedURLException ex) {
-           return ex.getMessage();
+            Logger.getLogger(TLS.class.getName()).log(Level.SEVERE, null, ex);
+            return ex.getMessage();
         } catch (IOException ex) {
+            Logger.getLogger(TLS.class.getName()).log(Level.SEVERE, null, ex);
            return ex.getMessage();
         }
-    
+         
     }
+    
+
     public static void main(String[] args) {
         System.out.println(testConnection());
     }
