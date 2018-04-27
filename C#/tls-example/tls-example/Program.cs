b35755a310fe4c8566f916c743167339b0831d3f
Leonardo Barbosa <leonardo.barbosa@pagar.me>
2018-04-27 11:04:40 -0300

    c#: add c# example request to tlsv1.2 endpoint

---
+++ C#/tls-example/tls-example/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace tlsexample
+{
+    class MainClass
+    {
+        private static String BASE_URL = "https://tls12.pagar.me";
+
+        public static void Main(string[] args)
+        {
+            var response = GetResponse();
+            if(!response.Equals(null))
+            {
+                Console.WriteLine(response);
+            }
+        }
+
+        private static String GetResponse()
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(BASE_URL);
+
+            request.UserAgent = "pagarme-net/tls-example";
+            request.Method = "GET";
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                return reader.ReadToEnd();
+            } catch(Exception e) {
+                Console.WriteLine("Got error! \n" + e.Message);
+            }
+
+            return null;
+        }
+    }
+}
